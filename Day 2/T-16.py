d484d748d756c9f0ac3d70c1eb57e6801d68774c
Yadish Shaikh <96179802+yadishh@users.noreply.github.com>
2022-08-23 23:15:48 +0530

    Create T-16.py

---
+++ Day 2/T-16.py
@@ -0,0 +1,12 @@
+class Student:
+
+    def __init__(self):
+        self.roll=101
+        self.name="Passion"
+
+    def show(self):
+        print(self.roll)
+        print(self.name)
+        
+s1=Student()
+s1.show()
