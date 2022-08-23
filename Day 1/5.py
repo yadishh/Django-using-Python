07884a5934f6513c7f179cdb11396c3940664930
Yadish Shaikh <96179802+yadishh@users.noreply.github.com>
2022-08-23 23:04:43 +0530

    Update 5.py

--- Day 1/5.py
+++ Day 1/5.py
@@ -1,5 +1,11 @@
-x=int(input("Enter the Age: "))
-if(x <=60):
-    print("Full ticket")
-elif(x>=60):
-    print("Half ticket")
+x=int(input("Enter the 1st number: "))
+y=int(input("Enter the 2st number: "))
+z=int(input("Enter the 3st number: "))
+if(x<z)and(y<z):
+    print(z,"is maximum")
+elif(y<x)and(z<x):
+    print(x,"is maximum")
+elif(x<y)and(z<y):
+    print(y,"is maximum")
+else:
+    print("All numbers are equal")
