dfa5dc04534b874cd1f9b08d8d28b7d917f9d0da
Yadish Shaikh <96179802+yadishh@users.noreply.github.com>
2022-08-23 23:06:20 +0530

    Create 10.py

---
+++ Day 1/10.py
@@ -0,0 +1,16 @@
+x=int(input("Enter the 1st number: "))
+y=int(input("Enter the 2st number: "))
+
+#x,y=y,x
+#print(x,y)
+
+#x=x+y
+#y=x-y
+#x=x-y
+#print(x,y)
+
+temp=0
+temp=x
+x=y
+y=temp
+print(x,y)
