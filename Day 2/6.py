9b7ee8b06449b78392985613dce5f967e8f9cc3e
Yadish Shaikh <96179802+yadishh@users.noreply.github.com>
2022-08-23 23:09:48 +0530

    Create 6.py

---
+++ Day 2/6.py
@@ -0,0 +1,11 @@
+mydict={
+          'roll':101,
+          'name':'Passion',
+          'city':'Jalgaon'
+       }
+
+print(type(mydict))
+print(mydict)
+print(mydict['roll'])
+print(mydict['name'])
+print(mydict['city'])
