ac02aab38607ed84a079f450f6c55de0ef04e3a5
Yadish Shaikh <96179802+yadishh@users.noreply.github.com>
2022-08-23 23:02:55 +0530

    Create 2.py

---
+++ Day 1/2.py
@@ -0,0 +1,8 @@
+x=int(input("Enter the 1st number: "))
+y=int(input("Enter the 2st number: "))
+if(x>y):
+    print(x,"is Maximum")
+    print(y,"is Minimum")
+else:
+    print(x,"is Minimum")
+    print(y,"is Maximum")
